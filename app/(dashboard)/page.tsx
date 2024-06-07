d6e6a4c1d3a05b24b62c4b55b8a02b7bf7767a88
JD John <ubunteen2@gmail.com>
2024-06-07 09:47:55 -0400

    Deployed to vercel

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,12 +2,14 @@
 
 import { Button } from "@/components/ui/button";
 import { useNewAccount } from "@/features/accounts/hooks/use-new-account";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function Home() {
   const { onOpen } = useNewAccount();
 
   return(
     <div>
+      <SpeedInsights />
       <Button onClick={onOpen}>
         Add an account
       </Button>
