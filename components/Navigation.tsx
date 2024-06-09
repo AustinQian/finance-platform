18722a7670604ab5b5fbdaebbec0510522a70345
JD John <ubunteen2@gmail.com>
2024-06-09 12:31:27 -0400

    Transaction page implemented, one can choose between income and expense

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -24,8 +24,8 @@ const routes =[
         label:'Overview',
     },
     {
-        href:'/transaction',
-        label:'Transaction',
+        href:'/transactions',
+        label:'Transactions',
     },
     {
         href:'/accounts',
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,17 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
-}
+};
+
+//Function below is to convert the amount from database to frontend actual amount
+export function converAmountFromMiliunites(amount: number) {
+  return amount / 1000;
+};
+
+
+//Function below is to convert the amount to store into the database
+export function converAmountToMiliunites(amount: number) {
+  return Math.round(amount * 1000);
+};
