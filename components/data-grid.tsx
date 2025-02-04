5dc53be6717062e265909f6c2cffca4a783a09ca
JD John <ubunteen2@gmail.com>
2025-02-04 11:29:23 -0500

    Updated main page content

--- components/data-grid.tsx
+++ components/data-grid.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import { useGetSummary } from "@/features/summary/api/use-get-summary";
+import { FaPiggyBank } from "react-icons/fa"
 import { formatDateRange } from "@/lib/utils";
 import { useSearchParams } from "next/navigation";
+import { DataCard } from "@/components/data-card";
 
 export const DataGrid = () => {
+    const { data } = useGetSummary();
+
     const params = useSearchParams();
     const to = params.get("to") || undefined;
     const from = params.get("from") || undefined;
@@ -12,7 +17,14 @@ export const DataGrid = () => {
 
     return (
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 pb-2 mb-8">
-            {dateRangeLabel}
+            <DataCard 
+                title="Remaining"
+                value={data?.remainingAmount}
+                percentageChange={data?.remainingChange}
+                icon={FaPiggyBank}
+                variant="default"
+                dateRange={dateRangeLabel}
+            />
         </div>
     )
 }
